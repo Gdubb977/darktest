4ba56051e6a987ffb7b78f8a0734608534281ae4
gdub <rundamile@gmail.com>
2024-08-11 13:47:55 -0600

    minor push

---
+++ lists_test/test_list.py
@@ -0,0 +1,32 @@
+
+
+#TODO
+#IMPORT YOUR LIST HERE 
+
+
+def main():
+    
+    #TODO
+    #use the imported list as the new list
+    new_list = None
+    
+    copy_list = new_list #do not touch 
+    
+    #TODO
+    #add a new fruit to the new_list
+
+    #TODO
+    #remove the 1st item from the list
+    
+    
+    #TODO
+    #add 'Dragon Fruit' to the 2nd to last item
+    
+    
+    
+    return copy_list, new_list
+
+
+
+if __name__ == '__main__':
+    main()
--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 import unittest
 import sys
 from variables_test.python_variables import main as  test_vars
+from lists_test.test_list import main as test_list
 class TestClasses(unittest.TestCase):
     def __init__(self):
         self.initiated = True
         self.quiz = None
+        self.list_count = 0
         
         
     @classmethod
@@ -12,6 +14,7 @@ class TestClasses(unittest.TestCase):
         self.quiz = test_vars()
         answers = [str, int, float, list, dict, tuple]
         failed = []
+        self.list_count = len(self.quiz[3])
         for key, answer in enumerate(self.quiz):
            
             if type(answer) == answers[key]:
@@ -20,13 +23,26 @@ class TestClasses(unittest.TestCase):
                 failed.append(answer)
                 print(failed)
                 raise Exception('Variable type does was incorrect')
-                sys.exit(0)
+            
             
         #self.assertEqual(self,self.quiz, answer)
         
         return
     
+    @classmethod
+    def tList(self):
+        self.quiz = test_list()
+        copy_list = self.quiz[0]
+        user_list = self.quiz[1]
+        """if copy_list == user_list:
+            raise('The List was not changed')"""
+    
+        if user_list[-2].lower() != 'dragon fruit':
+                    
+            raise('Dragon Fruit Not added')
 
+        print('Passed')
+        
 if __name__ == '__main__':
     options = ['variables', 'list', 'dict', 'classes']
     all_tests = TestClasses()
@@ -39,5 +55,8 @@ if __name__ == '__main__':
                 case 'variables':
                     all_tests.tVariables()
                     break
-                    
+            
+                case 'list':
+                    all_tests.tList()
+                    break
             
